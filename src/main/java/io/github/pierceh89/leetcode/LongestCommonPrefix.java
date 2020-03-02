2faa41a10660319f35817593ed30437329bb2b47
pierceh89 <pierceh89@gmail.com>
2020-03-02 23:47:47 +0900

    refactoring LongestCommonPrefix

--- src/main/java/io/github/pierceh89/leetcode/LongestCommonPrefix.java
+++ src/main/java/io/github/pierceh89/leetcode/LongestCommonPrefix.java
@@ -2,34 +2,26 @@ package io.github.pierceh89.leetcode;
 
 /**
  * 14. Longest Common Prefix
+ * Vertical scanning approach
+ * Time Complexity is O(S) where S is the sum of all strings in array
  */
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length==0) {
             return "";
         }
-        StringBuilder sb = new StringBuilder();
-        int i=0;
-        boolean cont = true;
-        while(true) {
+		StringBuilder sb = new StringBuilder();
+        for(int i=0; i < strs[0].length(); i++) {
             if (i >= strs[0].length()) {
-                break;
+                return sb.toString();
             }
             char c = strs[0].charAt(i);
             for(int j=1; j<strs.length; j++) {
-                if(i >= strs[j].length()) {
-                    cont=false;
-                    break;
-                }
-                if (c != strs[j].charAt(i)) {
-                    cont=false;
-                    break;
+                if ((i >= strs[j].length()) ||
+                    (c != strs[j].charAt(i))){
+                    return sb.toString();
                 }
             }
-            if(!cont) {
-                break;
-            }
-            i++;
             sb.append(c);
         }
         return sb.toString();
