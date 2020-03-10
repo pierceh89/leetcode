cb60e925d7dd33a90128acde48a47a6459421f5b
pierceh89 <pierceh89@gmail.com>
2020-03-11 02:17:37 +0900

    Updated solutions - LongestPalindromicSubstring, MergeKSortedLists

--- src/main/java/io/github/pierceh89/leetcode/LongestPalindromicSubstring.java
+++ src/main/java/io/github/pierceh89/leetcode/LongestPalindromicSubstring.java
@@ -2,35 +2,48 @@ package io.github.pierceh89.leetcode;
 
 /**
  * 5. Longest Palindromic Substring
+ * Dynamic Programming Solution
+ * Time Complexity O(n^2)
+ * Space Complexity O(n^2)
+ *
+ * There's even O(n) solution
+ * https://leetcode.com/problems/longest-palindromic-substring/discuss/304553/O(n)-solution-Manacher%27s-Algorithm
  */
 class LongestPalindromicSubstring {
-	// bruteforce
-	// O(n^3) time complexity
-	// O(1) space complexity
-	public String longestPalindrome(String s) {
-        if(s==null || s.isEmpty()) {
-            return "";
+    public String longestPalindrome(String s) {
+        if (s.length() < 2) {
+            return s;
         }
-        String longest = s.substring(0,1);
-        for(int start=0; start < s.length()-1; start++) {
-            for(int end=s.length(); end>start && longest.length() < end-start; end--) {
-                String sub=s.substring(start,end);
-                if(isPalindrome(sub) && sub.length() > longest.length()) {
-                    longest = sub;
+        int sz = s.length();
+        int maxlen = 1;
+        String maxstr = s.substring(0, 1);
+        Boolean[][] pal = new Boolean[sz][sz];
+        for (int i = 0; i < sz; i++) {
+            if (sz - i < maxlen) {
+                break;
+            }
+            for (int j = sz - 1; j >= i; j--) {
+                pal[i][j] = isPalindrome(i, j, pal, s);
+                if (pal[i][j] && j - i + 1 > maxlen) {
+                    maxlen = j - i + 1;
+                    maxstr = s.substring(i, j + 1);
                 }
             }
         }
-        
-        return longest;
+        return maxstr;
     }
-    
-    public boolean isPalindrome(String ss) {
-        int size = ss.length();
-        for(int i=0; i < size/2; i++) {
-            if(ss.charAt(i) != ss.charAt(size-i-1)) {
-                return false;
-            }
+
+    public boolean isPalindrome(int i, int j, Boolean[][] pal, String s) {
+        if (pal[i][j] != null) {
+            return pal[i][j];
         }
-        return true;
+        if (i == j) {
+            pal[i][j] = true;
+        } else if (j - i == 1) {
+            pal[i][j] = s.charAt(i) == s.charAt(j);
+        } else {
+            pal[i][j] = s.charAt(i) == s.charAt(j) && isPalindrome(i + 1, j - 1, pal, s);
+        }
+        return pal[i][j];
     }
 }
--- src/main/java/io/github/pierceh89/leetcode/MergeKSortedLists.java
+++ src/main/java/io/github/pierceh89/leetcode/MergeKSortedLists.java
@@ -7,39 +7,41 @@ import java.util.Queue;
 
 /**
  * 23. Merge k Sorted Lists
+ * Time Complexity is O(nlogk) where n is the total number of ListNode and k is the length of lists
+ * Space Complexity O(k)
  */
 class MergeKSortedLists {
-        public ListNode mergeKLists(ListNode[] lists) {
-            if(lists.length == 0) {
-                return null;
-            }
-            Queue<Integer> q = new PriorityQueue<>();
-            int i=0;
-            int size=lists.length;
-            ListNode iter=lists[i];
-            while(true) {
-                if (iter != null) {
-                    q.offer(iter.val);
-                    iter = iter.next;
-                } else {
-                    if (i < size-1) {
-                        iter = lists[++i];
-                    }
-                    else {
-                        break;
-                    }
-                }
-            }
-            if(q.isEmpty()) {
-                return null;
-            }
-            ListNode ret = new ListNode(q.poll());
-            ListNode cur = ret;
-            while(!q.isEmpty()) {
-                ListNode newNode = new ListNode(q.poll());
-                cur.next = newNode;
-                cur = cur.next;
-            }
-            return ret;
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
         }
+        Queue<ListNode> q = new PriorityQueue<>(lists.length, (v1, v2) -> {
+            if (v1.val < v2.val) {
+                return -1;
+            } else if (v1.val == v2.val) {
+                return 0;
+            } else {
+                return 1;
+            }
+        });
+        for (ListNode list : lists) {
+            if (list != null) {
+                q.offer(list);
+            }
+        }
+        if (q.isEmpty()) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (!q.isEmpty()) {
+            ListNode node = q.poll();
+            tail.next = node;
+            tail = tail.next;
+            if (node.next != null) {
+                q.offer(node.next);
+            }
+        }
+        return dummy.next;
+    }
 }
