35a96364ca879e9670991b23cec0e7be58bdad65
pierceh89 <pierceh89@gmail.com>
2020-03-28 13:31:28 +0900

    FindAllNumbersDisappearedInArray

---
+++ src/main/java/io/github/pierceh89/leetcode/FindAllNumbersDisappearedInArray.java
@@ -0,0 +1,44 @@
+package io.github.pierceh89.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllNumbersDisappearedInArray {
+    /*
+     * Time complexity O(n)
+     * Space complexity O(n) for appear array
+     */
+    public List<Integer> findDisappearedNumbersExtraSpace(int[] nums) {
+        int n = nums.length;
+        boolean[] appear = new boolean[n+1];
+        for(int value : nums) {
+            appear[value] = true;
+        }
+        List<Integer> ret = new ArrayList<>();
+        for(int i=1; i<n+1; i++) {
+            if(!appear[i]) {
+                ret.add(i);
+            }
+        }
+        return ret;
+    }
+
+    // without extra space and O(n) in runtime
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        for(int i=0; i<n; i++) {
+            int index = Math.abs(nums[i])-1;
+            if(nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        List<Integer> ret = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            if(nums[i] > 0) {
+                ret.add(i+1);
+            }
+        }
+        return ret;
+    }
+}
